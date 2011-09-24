1423ac71cd42990b22958cd75fdd175b56e290c9
figo <figo@figos-iMac.local>
2011-09-23 17:01:12 -0700

    Initial Commit

---
+++ XnewsLite/XnewsLiteAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XnewsLiteAppDelegate.h
+//  XnewsLite
+//
+//  Created by figo on 9/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XnewsLiteAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
